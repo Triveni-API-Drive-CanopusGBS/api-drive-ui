0b24bc285a07d6f7c4cccf3d8068a29e1c598518
Gangadhar <gangadhar.reddy@canopusgbs.com>
2024-07-09 23:44:38 +0530

    Added addUser page for adding new users

---
+++ api-drive-ui-adminprofile/src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <header className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Welcome to Triveni!!</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">Profile</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="/" id="userManagementDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                User Management
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="userManagementDropdown">
+                <li><Link className="dropdown-item" to="/adduser">Add User</Link></li>
+                <li><Link className="dropdown-item" to="/edituser">Edit User</Link></li>
+                <li><Link className="dropdown-item" to="/users">All User Info</Link></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
